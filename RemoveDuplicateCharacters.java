5c050c426a704b08ad04d84f832691a4a0acd328
RAHULJ2903 <90106579+RAHULJ2903@users.noreply.github.com>
2021-09-27 20:21:11 +0530

    Add files via upload

---
+++ RemoveDuplicateCharacters.java
@@ -0,0 +1,30 @@
+package week3day2;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicateCharacters {
+
+	public static void main(String[] args) {
+		String Name = "Paypal India";
+		//Removes Blank Spaces
+		String name =Name.replaceAll("\\s", ""); 
+		//Conversion to Character Array
+		char[] chArray = name.toCharArray();
+		Set<Character> charSet = new LinkedHashSet<Character>();
+		Set<Character> dupCharSet = new LinkedHashSet<Character>();
+		for (char c : chArray) {	
+		if(charSet.add(c)) {
+			charSet.add(c);
+		}else {
+			dupCharSet.add(c);
+		}
+		}
+		//Removal of common elements from both set
+		charSet.removeAll(dupCharSet);
+		System.out.println("Unique Characters present in the String are "+charSet);
+		
+
+	}
+
+}
